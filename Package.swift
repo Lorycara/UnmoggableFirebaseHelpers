258b8207b11c0d5ced42a79ecbd82808162ac8ef
Lorycara <lorycara62@gmail.com>
2025-01-31 09:02:53 +0100

    [Feature] Updated version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["UnmoggableFirebaseHelpers"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "7.0.0")
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "8.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
